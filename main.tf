062c4ab58cdd34f56e87d156ee30d8415f5c1d58
Danilo Preda <danilopreda@outlook.com>
2026-09-16 02:01:28 -0300

    Add Terraform cloud configuration for localstack

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  cloud {
+    organization = "dpredaconsulting"
+
+    workspaces {
+      name = "terraform-localstack-first"
+    }
+  }
+}
+
 resource "aws_s3_bucket" "meu_bucket_local" {
   bucket = "bucket-de-teste"
 }
